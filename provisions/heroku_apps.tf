778e06b5dab4b0af7d6f3a789d310b59674c5a1e
louisl98 <louis.laugier@etu-webschoolfactory.fr>
2018-11-22 15:06:00 +0100

    'test'

--- provisions/heroku_apps.tf
+++ provisions/heroku_apps.tf
@@ -1,7 +1,15 @@
-resource "heroku_app" "staging" {
-
+resource "heroku_pipeline" "pipeline" {
+  name = "devops"
 }
 
-resource "heroku_app" "prod" {
+resource "heroku_pipeline_coupling" "staging" {
+  app = "${heroku_app.staging.name}"
+  pipeline = "${heroku_pipeline.pipeline.id}"
+  stage = "staging"
+}
 
+resource "heroku_pipeline_coupling" "production" {
+  app = "${heroku_app.production.name}"
+  pipeline = "${heroku_pipeline.pipeline.id}"
+  stage = "production"
 }
