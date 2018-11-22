b7681fcd9be964554d808d9eb5c4ed16a77078b7
louisl98 <louis.laugier@etu-webschoolfactory.fr>
2018-11-22 14:06:13 +0100

    'test'

--- repository.tf
+++ repository.tf
@@ -1,8 +1,3 @@
-provider "github" {
-    token ="7732b56a82d54cedb71e9a177c6fd1132ca1c3d5"
-    organization ="devops-wsf-p2021"
-}
-
 resource "github_repository" "example" {
     name = "example"
     description = "my codebase"
